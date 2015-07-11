d277d1c0fd9d3f8d9e23bf692a00544f823576a2
drtran <ktran@bemach.com>
2015-07-11 07:31:16 -0400

    Added five more tests for bankAccountTest --- left as an exercise

--- tdd/src/test/account/bankAccountTest.cpp
+++ tdd/src/test/account/bankAccountTest.cpp
@@ -1,13 +1,34 @@
 #include "cute.h"
 #include "bankAccountTest.h"
 
-void thisIsAbankAccountTestTest() {
+void shouldAllowDeposit() {
 	ASSERTM("start writing tests", false);	
 }
 
+void shouldAllowWithdraw() {
+	ASSERTM("start writing tests", false);
+}
+
+void shouldAllowWithdrawWithPenalty() {
+	ASSERTM("start writing tests", false);
+}
+
+void shouldCalculateInterest() {
+	ASSERTM("start writing tests", false);
+}
+
+void shouldCalculateInterestRate() {
+	ASSERTM("start writing tests", false);
+}
+
+
 cute::suite make_suite_bankAccountTest(){
 	cute::suite s;
-	s.push_back(CUTE(thisIsAbankAccountTestTest));
+	s.push_back(CUTE(shouldAllowDeposit));
+	s.push_back(CUTE(shouldAllowWithdraw));
+	s.push_back(CUTE(shouldAllowWithdrawWithPenalty));
+	s.push_back(CUTE(shouldCalculateInterest));
+	s.push_back(CUTE(shouldCalculateInterestRate));
 	return s;
 }
 
